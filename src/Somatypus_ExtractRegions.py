9ef6e87eecba9134ca9ea78ded8522a68107e0dc
adrianbaezortega <adrianbaez91@gmail.com>
2016-05-13 18:27:21 +0100

    Changes for version 1.2
    Fix of duplicated calls, separate genotyping of indels

---
+++ src/Somatypus_ExtractRegions.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python
+
+# SOMATYPUS: A PLATYPUS-BASED VARIANT CALLING PIPELINE FOR CANCER DATA
+# Adrian Baez-Ortega, Transmissible Cancer Group, University of Cambridge
+# 11/05/2016
+
+# Somatypus_ExtractRegions.py
+# Extracts regions from a regions file, if they contain variants from the VCFs
+# Called by prepare_genotyping() and prepare_genotyping_indelflagged()
+
+# INPUT
+# exomeFile: file with the original regions in CHR:START-END format, one per line
+# allele1VCF: path to VCF with bi-allelic variants / 1st allele of multi-allelic SNPs
+# allele2VCF: path to VCF with 2nd allele of multi-allelic SNPs
+# allele3VCF: path to VCF with 3rd allele of multi-allelic SNPs
+# indelsVCF: path to VCF with indels (or "none", if excluded == 1)
+# outDir: path to (existing) output folder
+# excluded: logical value indicating if the variants are indel-excluded SNPs (1) or not (0)
+
+
+"""
+This script is used to extract the regions from a regions file that contain variants from four
+different VCF files, corresponding to: bi-allelic variants / 1st allele of multi-allelic variants;
+2nd allele of multi-allelic variants; 3rd allele of multi-allelic variants; and indels (or blank). 
+The regions are consequently output to four different files, according to the alleles they contain.
+"""
+
+
+import sys
+import os
+import re
+
+
+# If not 7 arguments: print help
+if len(sys.argv) != 8:
+    print '\nSomatypus_ExtractRegions.py: Extracts regions from a regions file, if they contain variants from the VCFs.'
+    print '                             The 4 VCF files correspond to: bi-allelic SNPs / 1st allele of'
+    print '                             multi-allelic SNPs; 2nd allele of multi-allelic SNPs; 3rd allele of'
+    print '                             multi-allelic SNPs; indels (not needed if using indel-excluded SNPs).'
+    print '                             The regions are output to 4 different files, one for each input VCF.'
+    print '                      Input: A file with the original regions in CHR:START-END format, one per line.'
+    print '                             Four VCF files.'
+    print '                             Path to (existing) output folder.'
+    print '                             Logical value indicating if the variants are indel-excluded SNPs (1; indels will not be used) or not (0).'
+    print '                      Usage: Somatypus_ExtractRegions.py /path/to/regions.txt /path/to/var1.vcf /path/to/var2.vcf /path/to/var3.vcf </path/to/var4.vcf|"none"> /path/to/outDir <0/1>\n'
+    sys.exit(0)
+
+
+script, exomeFile, allele1VCF, allele2VCF, allele3VCF, indelsVCF, outDir, excluded = sys.argv
+
+
+# Compose paths to output region files
+print '\nExome regions file:', exomeFile
+if int(excluded) == 0:
+    outFile1 = outDir + '/regions_allele1.txt'
+    outFile2 = outDir + '/regions_allele2.txt'
+    outFile3 = outDir + '/regions_allele3.txt'
+    outFileInd = outDir + '/regions_indels.txt'
+else:
+    outFile1 = outDir + '/regions_allele1_indelExcluded.txt'
+    outFile2 = outDir + '/regions_allele2_indelExcluded.txt'
+    outFile3 = outDir + '/regions_allele3_indelExcluded.txt'
+    outFileInd = outDir + '/blank'
+
+
+# Variables for counting and storing selected regions (exons)
+count1 = 0
+count2 = 0
+count3 = 0
+countInd = 0
+countExon = 0
+allele1 = {}
+allele2 = {}
+allele3 = {}
+indels = {}
+
+
+# Read positions into dictionaries
+print '\nReading file:', allele1VCF
+with open(allele1VCF, 'r') as vcf:
+    for line in vcf:
+        if not line.startswith('#'):
+            col = line.strip().split('\t')
+            chr = col[0]
+            pos = int(col[1])
+            if chr in allele1:
+                allele1[chr].append(pos)
+            else:
+                allele1[chr] = [pos]
+
+print 'Reading file:', allele2VCF
+with open(allele2VCF, 'r') as vcf:
+    for line in vcf:
+        if not line.startswith('#'):
+            col = line.strip().split('\t')
+            chr = col[0]
+            pos = int(col[1])
+            if chr in allele2:
+                allele2[chr].append(pos)
+            else:
+                allele2[chr] = [pos]
+
+print 'Reading file:', allele3VCF, '\n'
+with open(allele3VCF, 'r') as vcf:
+    for line in vcf:
+        if not line.startswith('#'):
+            col = line.strip().split('\t')
+            chrom = col[0]
+            pos = int(col[1])
+            if chrom in allele3:
+                allele3[chrom].append(pos)
+            else:
+                allele3[chrom] = [pos]
+                
+# Indels are read only if indel-excluded SNPs are not input
+if int(excluded) == 0:
+    print '\nReading file:', indelsVCF
+    with open(indelsVCF, 'r') as vcf:
+        for line in vcf:
+            if not line.startswith('#'):
+                col = line.strip().split('\t')
+                chr = col[0]
+                pos = int(col[1])
+                if chr in indels:
+                    indels[chr].append(pos)
+                else:
+                    indels[chr] = [pos]
+
+
+# Process regions and check if they contain variants
+with open(exomeFile, 'r') as exome, \
+     open(outFile1, 'w') as out1, open(outFile2, 'w') as out2, open(outFile3, 'w') as out3, \
+     open(outFileInd, 'w') as outInd:
+    
+    for exon in exome:
+        print 'Processing region', exon.strip()
+        countExon = countExon + 1
+        exonComp = exon.strip().split(':')
+        chrom = exonComp[0]
+        start = int(exonComp[1].split('-')[0])
+        end = int(exonComp[1].split('-')[1])
+        
+        # For positions in each allele dict: add exon to output file if it contains any position
+        if chrom in allele1:
+            for pos in allele1[chrom]:
+                if start <= pos and end >= pos:
+                    out1.write(exon)
+                    count1 = count1 + 1
+                    print ' Found in Allele 1 VCF'
+                    break
+    
+        if chrom in allele2:
+            for pos in allele2[chrom]:
+                if start <= pos and end >= pos:
+                    out2.write(exon)
+                    count2 = count2 + 1
+                    print ' Found in Allele 2 VCF'
+                    break
+        
+        if chrom in allele3:
+            for pos in allele3[chrom]:
+                if start <= pos and end >= pos:
+                    out3.write(exon)
+                    count3 = count3 + 1
+                    print ' Found in Allele 3 VCF'
+                    break
+        
+        # If not indel-excluded SNPs: search indels dict
+        if int(excluded) == 0:
+            if chrom in indels:
+                for pos in indels[chrom]:
+                    if start <= pos and end >= pos:
+                        outInd.write(exon)
+                        countInd = countInd + 1
+                        print ' Found in Indels VCF'
+                        break
+
+
+print '\n', countExon, 'regions processed'
+print count1, 'regions output to file', outFile1
+print count2, 'regions output to file', outFile2
+print count3, 'regions output to file', outFile3
+if int(excluded) == 0:
+    print countInd, 'regions output to file', outFileInd
+else:
+    print 'Indels not considered'
+print 'Done!\n'
--- src/Somatypus_VafFilter.py
+++ src/Somatypus_VafFilter.py
@@ -2,23 +2,23 @@
 
 # SOMATYPUS: A PLATYPUS-BASED VARIANT CALLING PIPELINE FOR CANCER DATA
 # Adrian Baez-Ortega, Transmissible Cancer Group, University of Cambridge
-# 22/01/2016
+# 13/05/2016
 
 # Somatypus_VafFilter.py
-# Discards variants with a VAF >0.9 in all samples from a Platypus output VCF file
-# Also discards indels with any flag other than PASS, and separates SNPs and indels to two different output files
+# Discards variants with a VAF >0.9 in all samples from two Platypus output VCF files
+# (SNPs and indels)
 # Called by merge_filter_all()
 
 # INPUT
-# vcfFile: path to VCF file
+# snpFile: path to SNPs VCF file
+# indFile: path to indels VCF file
 
 
 """
-This script is used to remove consistent high-VAF SNPs from a Platypus VCFs. 
+This script is used to remove consistent high-VAF SNPs from two Platypus VCFs. 
 For each call, it checks if the VAF (number of reads supporting variant / total reads)
 is more than 0.9 (by default) across all the samples, and in that case it discards the
-variant. Also discards indels with any flag other than PASS, and separates SNPs and
-indels to two different output files.
+variant.
 """
 
 import sys
@@ -30,66 +30,96 @@ import re
 MAXVAF = 0.9
 
 
-if len(sys.argv) != 2:
-    print '\nSomatypus_VafFilter.py: Discards variants with a VAF >0.9 in all samples from a Platypus output VCF file.'
-    print '                        Also discards indels with any flag other than PASS, and separates SNPs and indels.'
+if len(sys.argv) != 3:
+    print '\nSomatypus_VafFilter.py: Discards variants with a VAF >0.9 in all samples from two Platypus output VCF files'
+    print '                        (one for SNPs and one for indels).
     print '                        *All calls in the VCF must be biallelic (no commas in the ALT column).*'
-    print '                 Input: Path to VCF file.'
-    print '                 Usage: Somatypus_VafFilter.py /path/to/variants.vcf\n'
+    print '                 Input: Path to VCF files.'
+    print '                 Usage: Somatypus_VafFilter.py /path/to/snps.vcf /path/to/indels.vcf\n'
     sys.exit(0)
 
 
-script, vcfFile = sys.argv
+script, snpFile, indFile = sys.argv
 
 
-# Compose path of output VCF file
-outFileSNP = vcfFile[:-4] + '.VAFfilt_SNPs.vcf'
-outFileInd = vcfFile[:-4] + '.VAFfilt_Indels.vcf'
+# Compose path of output files
+outFileSNP = snpFile[:-4] + '.VAFfilt.vcf'
+outFileInd = indFile[:-4] + '.VAFfilt.vcf'
 
-print '\nInput file:     ', vcfFile
-print 'Output SNPs file:   ', outFileSNP
-print 'Output indels file: ', outFileInd
+print '\nInput files:    ', snpFile
+print '                ', indFile
+print 'Output files:   ', outFileSNP
+print '                ', outFileInd
   
 
+countSnp1 = 0
+countSnp2 = 0
+countInd1 = 0
+countInd2 = 0
 
-count1 = 0
-count2 = 0
-count3 = 0
-# Read every variant in the input VCF
-with open(vcfFile, 'r') as vcf, open(outFileSNP, 'w') as outSNP, open(outFileInd, 'w') as outInd:
-    for line in vcf:
+
+# Read every variant in the SNPs VCF
+with open(snpFile, 'r') as inSNP, open(outFileSNP, 'w') as outSNP:
+    for line in inSNP:
+        
         if line.startswith('#'):
             outSNP.write(line)
-            outInd.write(line)
+        
         else: 
-            count1 = count1 + 1
+            countSnp1 = countSnp1 + 1
             col = line.strip().split('\t')
             ref = col[3]
             alt = col[4]
             filter = col[6]
             data = col[9:]
-            # If it's an indel and has any flag other than PASS, skip
-            if len(ref) == len(alt) or filter == 'PASS':
-                for record in data:
-                    # Extract total reads (nr) and supp. reads (nv) for computing VAF
-                    nr = float(record.split(':')[4])
-                    nv = float(record.split(':')[5])
-                    if nr == 0:
-                        vaf = 1
-                    else:
-                        vaf = nv / nr
-                    # If VAF < threshold in any sample, write to corresponding output file
-                    if vaf <= MAXVAF:
-                        count2 = count2 + 1
-                        if len(ref) == len(alt):
-                            outSNP.write(line)
-                        else:
-                            outInd.write(line)
-                        break
-            else:
-                count3 = count3 + 1
+
+            for record in data:
+                # Extract total reads (nr) and supp. reads (nv) for computing VAF
+                nr = float(record.split(':')[4])
+                nv = float(record.split(':')[5])
+                if nr == 0:
+                    vaf = 1
+                else:
+                    vaf = nv / nr
+                # If VAF < threshold in any sample, write to corresponding output file
+                if vaf <= MAXVAF:
+                    countSnp2 = countSnp2 + 1
+                    outSNP.write(line)
+                    break
+             
+print '\n' + str(countSnp1 - countSnp2) + ' high-VAF SNPs discarded'
 
 
-print '\n' + str(count1 - count2 - count3) + ' high-VAF variants discarded'
-print count3, 'low-quality indels discarded' 
+# Read every variant in the indels VCF
+with open(indFile, 'r') as inInd, open(outFileInd, 'w') as outInd:
+    for line in inInd:
+        
+        if line.startswith('#'):
+            outInd.write(line)
+        
+        else: 
+            countInd1 = countInd1 + 1
+            col = line.strip().split('\t')
+            ref = col[3]
+            alt = col[4]
+            filter = col[6]
+            data = col[9:]
+
+            for record in data:
+                # Extract total reads (nr) and supp. reads (nv) for computing VAF
+                nr = float(record.split(':')[4])
+                nv = float(record.split(':')[5])
+                if nr == 0:
+                    vaf = 1
+                else:
+                    vaf = nv / nr
+                # If VAF < threshold in any sample, write to corresponding output file
+                if vaf <= MAXVAF:
+                    countInd2 = countInd2 + 1
+                    outInd.write(line)
+                    break
+             
+print '\n' + str(countInd1 - countInd2) + ' high-VAF indels discarded'
+
+
 print 'Done!\n'
