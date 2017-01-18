49b672bf81652df512732ca8c4dda85e03776890
adrianbaezortega <adrianbaez91@gmail.com>
2017-01-18 11:45:44 +0000

    fixed bug in median VAF calculation

--- src/Somatypus_IndelRescuedFilter.py
+++ src/Somatypus_IndelRescuedFilter.py
@@ -51,11 +51,13 @@ MINREADS = 3
 
 # Function for computing median
 def median(l):
-    half = len(l) // 2
-    l.sort()
-    if not len(l) % 2:
-        return (l[half - 1] + l[half]) / 2.0
-    return l[half]
+    if len(l) > 1:
+        half = len(l) // 2
+        l.sort()
+        if not len(l) % 2:
+            return (l[half - 1] + l[half]) / 2.0
+        return l[half]
+    return l[0]
 
 
 # Compose path to output VCF file
